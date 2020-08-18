e55add164864b4d1f37a88eba2a099c0adb3928d
pr3m19 <69632837+pr3m19@users.noreply.github.com>
2020-08-18 11:35:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ then
     echo "TRAVIS_TAG=${TRAVIS_TAG}"
     echo "Publishing to Docker Hub..."
     docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" && \
-    docker build -t shinmyung0/pullrequest-events-resource:${TRAVIS_TAG} -t shinmyung0/pullrequest-events-resource:latest . && \
-    docker push shinmyung0/pullrequest-events-resource:${TRAVIS_TAG} && \
-    docker push shinmyung0/pullrequest-events-resource:latest
+    docker build -t Pr3m-Test/pullrequest-events-resource:${TRAVIS_TAG} -t Pr3m-Test/pullrequest-events-resource:latest . && \
+    docker push Pr3m-Test/pullrequest-events-resource:${TRAVIS_TAG} && \
+    docker push Pr3m-Test/pullrequest-events-resource:latest
 fi
